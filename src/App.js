8c74854ce2487b9b7394bf0c73f123b93f740c6d
ramtin <ramtin.futures.trade@gmail.com>
2023-03-24 20:34:40 +0430

    basic 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import css from "./App.css"
+/* e.g for import icons
+import { FaBeer } from 'react-icons/fa';*/
+
 
 const App =()=>{
     return (
-      <div id='app' className="container">
-        
-      </div>
+      <>
+
+      </>
     )
   }
 
